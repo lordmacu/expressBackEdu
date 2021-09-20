2d2d39ac8aae003a1db33e6ff61894e926c129bb
Cristian garcia <cristian.garcia@pegasi.io>
2021-09-20 11:09:26 -0500

    fix shemas

---
+++ schemas/book.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var itemSchema = new Schema(
+  {
+    name: String,
+    contactForm: String,
+    address: String,
+    city: String,
+    country: {
+      type: Schema.Types.ObjectId,
+      ref: "Country",
+    },
+    phone: String,
+    email: String,
+    web: String,
+    active: Boolean,
+        idPlatform:Number
+
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = itemSchema;
